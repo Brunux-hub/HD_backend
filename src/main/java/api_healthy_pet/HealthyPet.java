1c448253b406620a701d207afabebf7337e071e7
Machado2728! <machadoadriano28@gmail.com>
2026-05-26 18:41:11 -0500

    Initial Project Structure

---
+++ src/main/java/api_healthy_pet/HealthyPet.java
@@ -0,0 +1,13 @@
+package api_healthy_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthyPet {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthyPet.class, args);
+	}
+
+}
