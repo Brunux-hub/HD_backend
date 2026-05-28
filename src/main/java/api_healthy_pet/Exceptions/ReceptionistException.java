5f7cf059ffe47b063075d5f60c3f405adaddb117
Machado2728! <machadoadriano28@gmail.com>
2026-05-28 01:44:24 -0500

    Creacion de Entity, Dtos, Exception, Repository, Mapper, Service, Controller para Receptionist

---
+++ src/main/java/api_healthy_pet/Exceptions/ReceptionistException.java
@@ -0,0 +1,7 @@
+package api_healthy_pet.Exceptions;
+
+public class ReceptionistException extends RuntimeException {
+    public ReceptionistException(String message) {
+        super(message);
+    }
+}
