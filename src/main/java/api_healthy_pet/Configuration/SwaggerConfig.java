5471e5fe2402c362fef54472ed20f1068f67cb4a
cristianvm-21 <avatec.advanced@gmail.com>
2026-06-01 17:49:09 -0500

    Agregando Swagger al Proyecto

---
+++ src/main/java/api_healthy_pet/Configuration/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package api_healthy_pet.Configuration;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
+
+@OpenAPIDefinition(
+        info = @Info(
+                title = "API HealthyPets",
+                description = "API REST para la gestión de servicios veterinarios, mascotas, propietarios y citas médicas.",
+                version = "1.0.0",
+                license = @License(
+                        name = "Uso académico"
+                )
+        )
+)
+public class SwaggerConfig {
+
+}
