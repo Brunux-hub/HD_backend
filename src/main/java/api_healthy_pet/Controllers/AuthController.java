d22661c71825d3088296c4fb5a1166e4e1aa331d
kriiss222 <requejogonzalezvalery@gmail.com>
2026-06-05 15:37:26 -0500

    feat: agregar CrossOrigin al AuthController

--- src/main/java/api_healthy_pet/Controllers/AuthController.java
+++ src/main/java/api_healthy_pet/Controllers/AuthController.java
@@ -1,5 +1,29 @@
-@PostMapping("/register")
-public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
-    authService.register(request);
-    return ResponseEntity.ok("success");
+package api_healthy_pet.Controllers;
+
+import api_healthy_pet.Dtos.Request.LoginRequest;
+import api_healthy_pet.Dtos.Request.RegisterRequest;
+import api_healthy_pet.Dtos.Response.LoginResponse;
+import api_healthy_pet.Services.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin(origins = "*")
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody RegisterRequest request) {
+        authService.register(request);
+        return ResponseEntity.ok("success");
+    }
 }
